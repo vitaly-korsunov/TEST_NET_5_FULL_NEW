aae76f652eb8a23401c330006f531b22668f9301
Vitaly <vitaly.korsunov@gmail.com>
2023-12-09 13:16:34 +0200

    Add project files.

---
+++ Dal/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+        IEnumerable<SelectListItem> GetAllDropDownList(string obj);
+    }
+}
---
+++ Web/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using Dal.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _prodRepo;
+        public ProductController(IProductRepository prodRepo)
+        {
+            _prodRepo = prodRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Upsert(int? id)
+        {
+
+            ProductVM productVM = new()
+            {
+                Product = new Product(),
+                CategorySelectList = _prodRepo.GetAllDropDownList("Category")
+            };
+            if (id == null)
+            {
+                // this is For create
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = await _prodRepo.Find(id.GetValueOrDefault());
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                return View(productVM);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                if (productVM.Product.Id == 0)
+                {
+                    // create
+                    await _prodRepo.Add(productVM.Product);
+
+                }
+                else
+                {
+                    _prodRepo.Update(productVM.Product);
+                }
+                await _prodRepo.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            productVM.CategorySelectList = _prodRepo.GetAllDropDownList("Category");
+
+            return View(productVM);
+        }
+
+        #region API CALL
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            IEnumerable<Product> productList = await _prodRepo.GetAll(includeProperties: "Category");
+
+            return Json(new { data = productList });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var obj = await _prodRepo.Find(id.GetValueOrDefault());
+            if (obj == null)
+            {
+                return Json(new { success = false, Message = "Error while deleting" });
+            }
+            await _prodRepo.Remove(obj);
+            await _prodRepo.Save();
+            return Json(new { success = true, Message = "Delete Successful" });
+        }
+        #endregion
+
+    }
+}
